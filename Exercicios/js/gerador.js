6dba54fbf0fd8b10fac168f967757556238b4081
edenilson <edenilson.souzaaaaa@gmail.com>
2023-05-11 19:43:20 -0400

    first commit

---
+++ Exercicios/js/gerador.js
@@ -0,0 +1,17 @@
+function tabuada() {
+    let saida = document.getElementById('saida')
+    let n = Number(document.getElementById('fnum').value)
+
+    saida.innerHTML = `<h2>Tabuada de ${n}</h2>`
+    if ( n <= 10 ) {
+    let c = 1
+    while (c <= 10) {
+        saida.innerHTML += `${n} x ${c} = <strong>${n * c}</strong><br>`
+        c ++
+    }
+}
+else {
+    saida.innerHTML +='Nao pode digitar maior que 10'
+}
+
+}
